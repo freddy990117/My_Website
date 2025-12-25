873d8cd01b289bbf98b20d1a9d677d795634e597
Freddy990117 <Freddy990117@gmail.com>
2025-12-25 14:09:04 +0800

    fix : fix NO_LCP error

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import Homepage from "./Components/Homepage";
 const App = () => {
   return (
-    <div>
+    <main id="main">
       <Homepage />
-    </div>
+    </main>
   );
 };
 
